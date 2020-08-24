54f7846eca84b6e2dfd2f6d012b617c70bac22db
shahzaib-gakhar <69382114+shahzaib-gakhar@users.noreply.github.com>
2020-08-24 07:54:21 -0700

    ARCommit

---
+++ English AR/Assets/Vuforia/Editor/Scripts/OpenSourceInitializer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9049f7a4a1954b5e78103de62b43e1d359afd325560b748aeb828b20bc8cbab2
+size 2795
---
+++ English AR/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3531816a2932db665ea9bad6cf8d9af49e781c91b23286c7aac79e12a3207625
+size 4172
