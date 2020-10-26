08fe848a4018aed2a2c53b771dcd569d0a7af739
shahzaib-gakhar <69382114+shahzaib-gakhar@users.noreply.github.com>
2020-10-26 11:02:13 -0700

    delete

--- English AR/Assets/3dmodels/fan/Probs/Ceiling Fan/Scripts/Fan_Rotation.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cc7f719971cf957ce1ed78ff6824547e846762e3e8c74a789b91b9ff251fdd6b
-size 314
--- English AR/Assets/Vuforia/Scripts/Internal/RuntimeOpenSourceInitializer.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:68c7f8a9f72d914d9bfa7216da46094332846045ac6d7ba17daccb17cbc2f9e1
-size 2658
